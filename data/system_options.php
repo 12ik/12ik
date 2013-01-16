02a27d124dacba09512a22855d85193178eac085
wang lijun <12ik.wang@gmail.com>
2013-01-16 17:50:56 +0800

    Signed-off-by: wang lijun <12ik.wang@gmail.com>

--- data/system_options.php
+++ data/system_options.php
@@ -3,7 +3,7 @@ defined('IN_IK') or die('Access Denied.');
 return array (
   'site_title' => '爱客网(12IK)开源社区',
   'site_subtitle' => '又一个爱客网(12IK)开源社区',
-  'site_url' => 'http://www.joyku.com/12ik/',
+  'site_url' => 'http://www.ik.com/',
   'site_email' => 'admin@admin.com',
   'site_icp' => '正在备案中',
   'isface' => '0',
