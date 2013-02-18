c51ce4a07f50c1163cdfe8f222563bbf04334254
wang lijun <12ik.wang@gmail.com>
2013-02-18 10:48:51 +0800

    Signed-off-by: wang lijun <12ik.wang@gmail.com>

--- install/action/next.php
+++ install/action/next.php
@@ -1,9 +1,9 @@
 <?php
 defined ( 'IN_IK' ) or die ( 'Access Denied.' );
 /*
- * 12ik爱客网 安装程序
- * @copyright (c) 2012-3000 12IK All Rights Reserved
- * @author wanglijun
+ * IKPHP爱客网 安装程序
+ * @copyright (c) 2012-3000 IKPHP All Rights Reserved
+ * @author 小麦
  * @Email:160780470@qq.com
  */
 
